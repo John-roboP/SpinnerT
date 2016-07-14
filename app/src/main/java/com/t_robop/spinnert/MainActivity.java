c6187801296112345143931bf16342623a79aea6
Ryo <r.otaka@outlook.jp>
2016-07-14 17:33:12 +0900

    ４７都道府県打ち込み完了

--- app/src/main/java/com/t_robop/spinnert/MainActivity.java
+++ app/src/main/java/com/t_robop/spinnert/MainActivity.java
@@ -18,8 +18,9 @@ public class MainActivity extends AppCompatActivity {
     Spinner Spshikoku;//四国6
     Spinner Spkyuushu;//九州7
 
-    int spHantei=0;//地方判定
-    String selected;
+    int spHantei=0;//地方判定↑
+    String selected;//地方判定用
+    String City;//県判定用
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -46,12 +47,6 @@ public class MainActivity extends AppCompatActivity {
 
 
 
-    //    if(selected.equals("東北")){
-      //      Sphokkai.setVisibility(View.VISIBLE);
-
-       // }else{
-       //     Log.d("test","test");
-       // }
 
         mSpinner1.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
             @Override
@@ -187,20 +182,141 @@ public class MainActivity extends AppCompatActivity {
 
         switch (spHantei){
             case 0:
+               City = (String) Sphokkai.getSelectedItem();//取得
+                if (City.equals("札幌")){
+
+                }else if(City.equals("函館")){
+
+                }else{
+                    //釧路
+                }
+
                 break;
             case 1:
+                City = (String) Sptohoku.getSelectedItem();
+                if (City.equals("青森")){
+
+                }else if(City.equals("岩手")){
+
+                }else if(City.equals("宮城")){
+
+                }else if(City.equals("山形")){
+
+                }else{
+                    //福島
+                }
+
                 break;
             case 2:
+                City = (String) Spkanto.getSelectedItem();
+                if (City.equals("茨城")){
+
+                }else if(City.equals("栃木")){
+
+                }else if(City.equals("群馬")){
+
+                }else if(City.equals("埼玉")){
+
+                }else if(City.equals("千葉")){
+
+                }else if(City.equals("東京")){
+
+                }else{
+                    //神奈川
+                }
+
                 break;
             case 3:
+                City = (String) Spchubu.getSelectedItem();
+                if (City.equals("山梨")){
+
+                }else if(City.equals("長野")){
+
+                }else if(City.equals("新潟")){
+
+                }else if(City.equals("富山")){
+
+                }else if(City.equals("石川")){
+
+                }else if(City.equals("福井")){
+
+                }else if(City.equals("静岡")){
+
+                }else if(City.equals("愛知")){
+
+                }else if(City.equals("岐阜")){
+
+                }else{
+                    //三重
+                }
+
                 break;
             case 4:
+                City = (String) Spkansai.getSelectedItem();
+                if (City.equals("滋賀")){
+
+                }else if(City.equals("京都")){
+
+                }else if(City.equals("大阪")){
+
+                }else if(City.equals("兵庫")){
+
+                }else if(City.equals("奈良")){
+
+                }else{
+                    //和歌山
+                }
+
                 break;
             case 5:
+                City = (String) Spchugoku.getSelectedItem();
+                if (City.equals("鳥取")){
+
+                }else if(City.equals("島根")){
+
+                }else if(City.equals("岡山")){
+
+                }else if(City.equals("広島")){
+
+                }else{
+                    //山口
+                }
+
                 break;
             case 6:
+                City = (String) Spshikoku.getSelectedItem();
+                if (City.equals("香川")){
+
+                }else if(City.equals("愛媛")){
+
+                }else if(City.equals("徳島")){
+
+                }else {
+                    //高知
+
+                }
+
                 break;
             case 7:
+                City = (String) Spkyuushu.getSelectedItem();
+                if (City.equals("福岡")){
+
+                }else if(City.equals("佐賀")){
+
+                }else if(City.equals("長崎")){
+
+                }else if(City.equals("熊本")){
+
+                }else if(City.equals("大分")){
+
+                }else if(City.equals("宮崎")){
+
+                }else if(City.equals("鹿児島")){
+
+                }else{
+                    //沖縄
+                }
+
                 break;
         }
 
